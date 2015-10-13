d2a64f7c20fae72ffbaf23242bef7bbf8af96a2f
Allan SIMON <allan.simon@twenga.com>
2015-10-13 18:22:25 +0000

    golint

--- campaign_extension_setting.go
+++ campaign_extension_setting.go
@@ -5,29 +5,56 @@ import (
 	"fmt"
 )
 
+// CampaignExtensionSettingService (v201506)
+// Service used to manage extensions at the campaign level.
+// The extensions are managed by AdWords using existing feed services,
+// including creating and modifying feeds, feed items,
+// and campaign feeds for the user.
+//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService
 type CampaignExtensionSettingService struct {
 	Auth
 }
 
+// CampaignExtensionSetting is used to add or
+// modify extensions being served for the specified campaign.
+//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService.CampaignExtensionSetting
 type CampaignExtensionSetting struct {
 	CampaignID       int64             `xml:"campaignId,omitempty"`
 	ExtensionType    string            `xml:"extensionType"`
 	ExtensionSetting *ExtensionSetting `xml:"extensionSetting"`
 }
 
+// ExtensionSetting specifies when and which extensions should serve
+// at a given level (customer, campaign, or ad group).
+//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService.ExtensionSetting
 type ExtensionSetting struct {
 	Extensions            []ExtensionFeedItem `xml:"extensions"`
 	PlateformRestrictions string              `xml:"platformRestrictions"`
 }
 
+// CampaignExtensionSettingOperations is a conveniency map on CampaignExtensionSetting
+// to manipulate
+// it can have the 3 following keys (case sensitive)
+//
+// ADD
+// SET
+// REMOVE
 type CampaignExtensionSettingOperations map[string][]CampaignExtensionSetting
 
+// NewCampaignExtensionSettingService is a constructor for CampaignExtensionSettingService
 func NewCampaignExtensionSettingService(auth *Auth) *CampaignExtensionSettingService {
 	return &CampaignExtensionSettingService{Auth: *auth}
 }
 
+// Get returns an array of CampaignExtensionSettings' and
+// the total number of CampaignExtensionSettings' matching the selector.
+//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService#get
 func (s *CampaignExtensionSettingService) Get(selector Selector) (
-	extensionSettings []ExtensionSetting,
+	extensionSettings []CampaignExtensionSetting,
 	totalCount int64,
 	err error,
 ) {
@@ -51,8 +78,8 @@ func (s *CampaignExtensionSettingService) Get(selector Selector) (
 		return extensionSettings, totalCount, err
 	}
 	getResp := struct {
-		Size              int64              `xml:"rval>totalNumEntries"`
-		ExtensionSettings []ExtensionSetting `xml:"rval>entries"`
+		Size              int64                      `xml:"rval>totalNumEntries"`
+		ExtensionSettings []CampaignExtensionSetting `xml:"rval>entries"`
 	}{}
 	err = xml.Unmarshal([]byte(respBody), &getResp)
 	if err != nil {
@@ -62,9 +89,10 @@ func (s *CampaignExtensionSettingService) Get(selector Selector) (
 
 }
 
+// Mutate allows you to add, modify and remove CampaignExtensionSetting, returning the
+// modified ones.
 //
-//
-//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService#mutate
 func (s *CampaignExtensionSettingService) Mutate(
 	campaignExtensionSettingOperations CampaignExtensionSettingOperations,
 ) (campaignExtensionSettings []CampaignExtensionSetting, err error) {
@@ -112,10 +140,10 @@ func (s *CampaignExtensionSettingService) Mutate(
 	return mutateResp.CampaignExtensionSettings, err
 }
 
-// Relevant documentation
+// Query allows to use AWQL to Get CampaignExtensionSettings matching
+// the query
 //
-//     https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService#query
-//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService#query
 func (s *CampaignExtensionSettingService) Query(query string) (campaignExtensionSettings []CampaignExtensionSetting, totalCount int64, err error) {
 
 	respBody, err := s.Auth.request(
--- extension_feed_item.go
+++ extension_feed_item.go
@@ -1,5 +1,7 @@
 package gads
 
+// CommonExtensionFeedItem is the Parent type for
+// all ExtensionFeedItem
 type CommonExtensionFeedItem struct {
 	Type       string  `xml:"xsi:type,attr,omitempty"`
 	FeedID     int64   `xml:"feedId,omitempty"`
@@ -14,19 +16,25 @@ type CommonExtensionFeedItem struct {
 	// end common
 }
 
+// ExtensionFeedItem is an interface all extension feed item implements
 type ExtensionFeedItem interface {
 	GetFeedID() int64
 	GetFeedItemID() int64
 }
 
+// GetFeedID returns the ID of the feed this item belongs to
 func (e *CommonExtensionFeedItem) GetFeedID() int64 {
 	return e.FeedID
 }
 
+// GetFeedItemID returns the ID of this feed item
 func (e *CommonExtensionFeedItem) GetFeedItemID() int64 {
 	return e.FeedItemID
 }
 
+// SitelinkFeedItem represents a sitelink extension.
+//
+// see https://developers.google.com/adwords/api/docs/reference/v201506/CampaignExtensionSettingService.SitelinkFeedItem
 type SitelinkFeedItem struct {
 	*CommonExtensionFeedItem
 	Text                string   `xml:"sitelinkText"`
